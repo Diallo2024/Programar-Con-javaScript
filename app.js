b1c5efc5d8e7ff3ec9d088b9444ce95af19cb31c
Diallo2024 <urymamadu@gmail.com>
2024-09-27 23:12:43 -0700

    otra forma de sumar y restar valor a una variable

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-// sumar dos variables
+// Restar dos variables
 let alumnos = 50;
 let alumnas = 30;
 
-let totalAlumnos = alumnas + alumnos;
+let totalAlumnos = alumnas - alumnos;
 console.log(totalAlumnos);
 
+//Suma y resta de un numero de una variable.
+
+let naranjas = 10;
+
+naranjas -= 3;
+console.log(naranjas);
+
+naranjas += 5;
+console.log(naranjas);
+
+let numberOfLikes = 8;
+numberOfLikes ++;
+console.log(numberOfLikes);
+numberOfLikes --;
+
+console.log(numberOfLikes);
