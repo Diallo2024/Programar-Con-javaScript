40f4aaa57e1e314beeef7a5f0867836f0ded4619
Diallo2024 <urymamadu@gmail.com>
2024-09-27 23:33:49 -0700

    el opedrador / (dividir)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let productCost = 30;
-let numberOfProducts = 5;
 
-let totalCost = productCost * numberOfProducts;
-console.log(totalCost);
+let numberOfProducts = 5;
+let totalCost = 80;
+
+let averageCostPerProduct = totalCost / numberOfProducts;
+console.log(averageCostPerProduct);
