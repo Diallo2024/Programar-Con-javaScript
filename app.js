0057491a71297ab9b6dfc092afdb81c31a2cc6cf
Diallo2024 <urymamadu@gmail.com>
2024-09-27 22:39:15 -0700

    declaracion e inicializar variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// Declare variables and modify their values
+
+let numberOfCats = 2;
+let numberOfDogs = 4;
+
+console.log(numberOfCats, numberOfDogs);
+
+// Modificar el valor de una variable
+
+numberOfCats = 4; // Esta sería la forma más simple de hacerlo, reasignando le otro valor a la variable.
+ 
