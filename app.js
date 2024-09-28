14ef704f4a8caceea4dac6fdd8475a24add181f7
Diallo2024 <urymamadu@gmail.com>
2024-09-28 16:34:02 -0700

    stroring data with types

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ const horasPorDía = 24;
 const minutosPorHora = 60;
 const segundosPorMinuto = 60;
 
+// Almacenar datos segun el tipo de dato.
+
+let positiveInteger = 22;
+let negativeInteger = -12;
+let largeDecimal = 10.434345454546;
+
+console.log(typeof positiveInteger); // number
+console.log(typeof negativeInteger); // number
+console.log(typeof largeDecimal); // float number
+
+let userIsSignedIn = true;
+let userIsAdmin = false;
+
+console.log(typeof userIsSignedIn); // boolean
+console.log(typeof userIsAdmin); // boolean
+
+let firstname = "David";
+let lastName = 'Handerson';
+
+console.log(typeof firstname); // string
+console.log(typeof lastName); // string
+
+console.log(firstname + ' ' + lastName); // concatenating variables with a + operator
+
+let episodeTitle = 'pilote';
+let episodeDuration = 45;
+let hasBeenWatched = true;
+
+console.log(episodeTitle + ' ' + episodeDuration + ' ' + hasBeenWatched);
