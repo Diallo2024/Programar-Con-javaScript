52a6ebe76f129cd6a78894099461588393198889
Diallo2024 <urymamadu@gmail.com>
2024-09-27 23:28:55 -0700

    el orperador de multiplicar *

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-// Restar dos variables
-let alumnos = 50;
-let alumnas = 30;
+let productCost = 30;
+let numberOfProducts = 5;
 
-let totalAlumnos = alumnas - alumnos;
-console.log(totalAlumnos);
-
-//Suma y resta de un numero de una variable.
-
-let naranjas = 10;
-
-naranjas -= 3;
-console.log(naranjas);
-
-naranjas += 5;
-console.log(naranjas);
-
-let numberOfLikes = 8;
-numberOfLikes ++;
-console.log(numberOfLikes);
-numberOfLikes --;
-
-console.log(numberOfLikes);
+let totalCost = productCost * numberOfProducts;
+console.log(totalCost);
