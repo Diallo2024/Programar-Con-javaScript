a0b8c00e235c100effe35d136cd7f7069cb176d5
Diallo2024 <urymamadu@gmail.com>
2024-10-06 21:21:16 -0700

    While loop

--- app.js
+++ app.js
@@ -375,4 +375,16 @@ class Episode {
 
   for (let episode of episodes) {
     episode.hasBeenWatched = false;
+  };
+
+  let seatsLeft = 10;
+  let passengersStillToBoard = 8;
+  let passengersBoarded = 0;
+
+  while (seatsLeft > 0 && passengersStillToBoard > 0) {
+    passengersBoarded++;
+    seatsLeft--;
+    passengersBoarded--;
   }
+
+  console.log(passengersBoarded + ' successfully on board!');
